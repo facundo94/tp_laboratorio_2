9852b248e2ee64b1efa48d7a6d3aa4e56997965c
facundo94 <facu.31@live.com>
2017-06-13 05:43:14 -0300

    Add files via upload

---
+++ EntidadesAbstractas/Persona.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+using Excepciones;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+    {
+        #region fields
+
+        private string _apellido;
+        private int _dni;
+        private ENacionalidad _nacionalidad;
+        private string _nombre;
+
+        #endregion
+
+        #region Properties
+
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set
+            {
+                if (this.ValidarNombreApellido(value) != "")
+                    this._apellido = value;
+            }
+        }
+
+        public int DNI
+        {
+            get { return this._dni; }
+            set { this._dni = value; }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get { return this._nacionalidad; }
+            set { this._nacionalidad = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set
+            {
+                if (this.ValidarNombreApellido(value) != "")
+                    this._nombre = value;
+            }
+        }
+
+        public string StringToDNI
+        {
+            set
+            {
+                //try
+                //{
+                this.DNI = this.ValidarDni(this.Nacionalidad, value);
+                //}
+                //catch (NacionalidadInvalidaException ex)
+                //{
+                //    //throw new NacionalidadInvalidaException();
+                //    Console.WriteLine(ex.Message);
+                //}
+                //catch (DniInvalidoException ex)
+                //{
+                //    /*throw new DniInvalidoException*/Console.WriteLine(ex.Message);
+                //}                
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Persona() { }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+            : this()
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.DNI = dni; ;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.StringToDNI = dni;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("NOMBRE COMPLETO: ");
+            sb.AppendLine(this.Apellido + ", " + this.Nombre);
+            sb.Append("NACIONALIDAD: ");
+            sb.AppendLine(this.Nacionalidad.ToString());
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// El dato es valido si este es mayor a 0 y menor a 90000000, y la nacionalidad es 'Argentino' o
+        /// si el dato es mayor a 90000000 y la nacionalidad es 'Extranjero'
+        /// </summary>
+        /// <param name="nacionalidad">En base a este dato se determina si el dni ingresado es correcto</param>
+        /// <param name="dato">dato a validar</param>
+        /// <returns></returns>
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            if (dato > 0)
+            {
+                if (dato > 90000000)
+                {
+                    if (nacionalidad == ENacionalidad.Argentino)
+                        throw new DniInvalidoException("El numero exede al maximo permitido.");
+                }
+                else if (nacionalidad == ENacionalidad.Extranjero)
+                    throw new NacionalidadInvalidaException("La nacionalidad no coincide con el número de DNI");
+            }
+            else
+                throw new DniInvalidoException("El numero esta por debajo del minimo permitido.");
+
+            return dato;
+        }
+
+        /// <summary>
+        /// El dato es valido si este es mayor a 0 y menor a 90000000, y la nacionalidad es 'Argentino' o
+        /// si el dato es mayor a 90000000 y la nacionalidad es 'Extranjero'
+        /// </summary>
+        /// <param name="nacionalidad">En base a este dato se determina si el dni ingresado es correcto</param>
+        /// <param name="dato">dato a validar</param>
+        /// <returns></returns>
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int salida;
+            if (!int.TryParse(dato, out salida))
+                throw new DniInvalidoException("El DNI contiene caracteres invalidos.");
+
+            return this.ValidarDni(nacionalidad, salida);
+        }
+
+        /// <summary>
+        /// Valida que el string recibido solo contenga letras
+        /// </summary>
+        /// <param name="dato">string a validar</param>
+        /// <returns></returns>
+        private string ValidarNombreApellido(string dato)
+        {
+            Regex reg = new Regex(@"[^a-zA-ZñÑ]");
+
+            if (!reg.IsMatch(dato))
+                return dato;
+
+            return "";
+        }
+
+        #endregion
+
+        #region Nested Types
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        #endregion
+    }
+}
---
+++ EntidadesInstanciables/Profesor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EntidadesAbstractas;
+
+namespace EntidadesInstanciables
+{
+    public sealed class Profesor : Universitario
+    {
+        #region Fields
+
+        private Queue<Universidad.ECLases> _clasesDelDia;
+        private static Random _random;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Agrega dos EClase elegidas de forma aleatorea a la lista de clases
+        /// </summary>
+        private void _randomClases()
+        {
+            int i = 0;
+            for (i = 0; i < 2; i++)
+                this._clasesDelDia.Enqueue((Universidad.ECLases)Profesor._random.Next(0, 3));
+        }
+
+        /// <summary>
+        /// Retorna un string con todos los datos del Profesor
+        /// </summary>
+        /// <returns></returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(base.MostrarDatos());
+            sb.AppendLine(this.ParticiparEnClase());
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Un Profesor es igual a una EClase si, y solo si, el Profesor da esa EClase
+        /// </summary>
+        /// <param name="i">Profesor a comparar</param>
+        /// <param name="clase">EClase a comparar</param>
+        /// <returns></returns>
+        public static bool operator ==(Profesor i, Universidad.ECLases clase)
+        {
+            if (!object.ReferenceEquals(i, null) && !object.ReferenceEquals(clase, null))
+            {
+                foreach (EntidadesInstanciables.Universidad.ECLases e in i._clasesDelDia)
+                {
+                    if (e == clase)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Un Profesor no es igual a una EClase si, y solo si, el Profesor no da esa EClase
+        /// </summary>
+        /// <param name="i">Profesor a comparar</param>
+        /// <param name="clase">EClase a comparar</param>
+        /// <returns></returns>
+        public static bool operator !=(Profesor i, Universidad.ECLases clase)
+        {
+            return !(i == clase);
+        }
+
+        /// <summary>
+        /// Retorna un string indicando las clases que puede dar
+        /// </summary>
+        /// <returns></returns>
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CLASES DE DÍA: ");
+            foreach (EntidadesInstanciables.Universidad.ECLases i in this._clasesDelDia)
+            {
+                sb.AppendLine(i.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        public Profesor()
+        { }
+
+        static Profesor()
+        {
+            Profesor._random = new Random();
+        }
+
+        public Profesor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this._clasesDelDia = new Queue<Universidad.ECLases>();
+            this._randomClases();
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        #endregion
+
+    }
+}
